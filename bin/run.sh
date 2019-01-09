40bc99902386b3980996857e023f78d32e7b5266
Danilo Castro <daniloster@gmail.com>
2019-01-09 23:58:46 +0000

    Removed the extra comments on run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,17 +1,11 @@
 set -e
 set -u
 CURRENT_DIR="$(pwd)"
-# unit tests
-# pipenv run python -m unittest discover -p "test_*.py"
+# Coverage generation based on unit tests
 (CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage run --source "$CURRENT_DIR" --branch --omit="$CURRENT_DIR/pytoast/tests/coverage/*,$CURRENT_DIR/pytoast/tests/*,$CURRENT_DIR/setup.py,.venv/lib/python3.6/site-packages/*" -m  unittest discover -p "test_*.py")
-# Coverage generation
-# (CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage run --source "$CURRENT_DIR" --branch --omit="$CURRENT_DIR/pytoast/tests/coverage/*,$CURRENT_DIR/pytoast/tests/*,$CURRENT_DIR/setup.py,.venv/lib/python3.6/site-packages/*" $CURRENT_DIR/pytoast/tests/coverage/all_success/run.py --root="$CURRENT_DIR" --features="$CURRENT_DIR/pytoast/tests/coverage/all_success/features" --verbose)
-# (CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage run --append --source "$CURRENT_DIR" --branch --omit="$CURRENT_DIR/pytoast/tests/coverage/*,$CURRENT_DIR/pytoast/tests/*,$CURRENT_DIR/setup.py,.venv/lib/python3.6/site-packages/*" $CURRENT_DIR/pytoast/tests/coverage/one_fail/run.py --root="$CURRENT_DIR" --features="$CURRENT_DIR/pytoast/tests/coverage/one_fail/features" --verbose) || echo 'Skipping failure scenarios for coverage'
-# (CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage run --append --source "$CURRENT_DIR" --branch --omit="$CURRENT_DIR/pytoast/tests/coverage/*,$CURRENT_DIR/pytoast/tests/*,$CURRENT_DIR/setup.py,.venv/lib/python3.6/site-packages/*" $CURRENT_DIR/pytoast/tests/coverage/one_fail/run.py --root="$CURRENT_DIR" --features="$CURRENT_DIR/pytoast/tests/coverage/one_fail/features" --verbose --fail-fast) || echo 'Skipping failure scenarios for coverage with --fail-fast'
 # Reporting coverage
 CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage report
 CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage html -d .coverage_html
-
 
 function finish {
   cd "$CURRENT_DIR"
