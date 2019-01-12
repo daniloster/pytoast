7b4396a33be0a29e8c1986a848b1fe88323cd7aa
Danilo Castro <daniloster@gmail.com>
2019-01-12 16:16:19 +0000

    [release=patch] updating release mechanism

--- bin/release.sh
+++ bin/release.sh
@@ -6,13 +6,29 @@ function get_version {
   cat setup.py | grep "version" | cut -d "'" -f 2 | grep "."
 }
 
-function bump {
+# Validating if it is a PR
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "[ci] we are in a pull request, not releasing"
+  exit 0
+fi
+
+# Checking if it is a master commit with release attribute
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  echo '[ci] Setting github user'
+  git config --global user.email "daniloster@gmail.com"
+  git config --global user.name "Danilo Castro"
+  git remote add gh-publish "https://${GIT_AUTH_TOKEN}@github.com/daniloster/pytoast.git"
+  git fetch gh-publish
+  git checkout master
+  git rebase gh-publish/master
+
+  export TYPE_RELEASE="$(git log --no-merges -n 1 --pretty=%B | grep '\[release=' | awk '{print $1}' | sed s/release=// | sed s/[][]//g)"
+
   if [ "$TYPE_RELEASE" == "no-release" ]; then
-    echo "- We are skipping releasing"
+    echo "[ci] we are skipping releasing"
     exit 0
   fi
 
-  export TYPE_RELEASE="$(git log --no-merges -n 1 --pretty=%B | grep '\[release=' | awk '{print $1}' | sed s/release=// | sed s/[][]//g)"
   CURRENT_VERSION="$(get_version)"
   pipenv run bumpversion --current-version $CURRENT_VERSION $TYPE_RELEASE setup.py
   git add setup.py
@@ -24,35 +40,14 @@ function bump {
   echo "[skip ci] [release]: v${RELEASE_VERSION}"
   git commit -m "[skip ci] [release]: v${RELEASE_VERSION}"
   git tag -a "v${RELEASE_VERSION}"
-  git push gh-publish master --tags
-}
 
-function release {
-  rm -rf dist/
-  rm -rf bdist_wheel/
+  echo "[ci] cleaning dist/ build/ and bdist_wheel/"
+  rm -rf dist/ build/ bdist_wheel/
 
+  echo "[ci] Building setup.py"
   pipenv run python setup.py sdist bdist_wheel
+  echo "[ci] Uploading pytoast"
   pipenv run twine upload dist/*
-}
-
-# Validating if it is a PR
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "- We are in a pull request, not releasing"
-  exit 0
-fi
-
-# Checking if it is a master commit with release attribute
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  echo '** Setting github user'
-  git config --global user.email "daniloster@gmail.com"
-  git config --global user.name "Danilo Castro"
-  git remote add gh-publish "https://${GIT_AUTH_TOKEN}@github.com/daniloster/pytoast.git"
-  git fetch gh-publish
-  git checkout master
-  git rebase gh-publish/master
-
-  bump()
-  release()
 fi
 
 
