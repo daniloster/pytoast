1bfbabb741ddc965806302a60d860c9387b9bf72
Danilo Castro <daniloster@gmail.com>
2019-01-12 15:45:54 +0000

    [release=patch] updated gitignore

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,10 @@
 set -e
 set -u
 CURRENT_DIR="$(pwd)"
+
 # Coverage generation based on unit tests
 (CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage run --source "$CURRENT_DIR" --branch --omit="$CURRENT_DIR/pytoast/tests/coverage/*,$CURRENT_DIR/pytoast/tests/*,$CURRENT_DIR/setup.py,.venv/lib/python3.6/site-packages/*" -m  unittest discover -p "test_*.py")
+
 # Reporting coverage
 CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage report
 CURRENT_DIR="$(pwd)" && PYTHONPATH="$CURRENT_DIR" pipenv run python -m coverage html -d .coverage_html
